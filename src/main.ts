5ab2596f38b5b045e33bab9fbf732a7cdca1aee7
Matthew <12hughesm@gmail.com>
2023-11-30 10:26:49 +0000

    Updated addPokemonCard

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ const addPokemonCard = (pokemon : Pokemon) => {
         <div class="card__content">
             <h1 class="card__heading">${pokemon.name}</h1>
             <p class="card__text">
-            ${pokemon.name} (${pokemon.id}) is a ${pokemon.types} type pokemon.
+            ${pokemon.name} (#${pokemon.id}) is a ${pokemon.types.join(' & ')} type pokemon.
             </p>
         </div>
     </div>`
