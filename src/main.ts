2b5a2627bb70a3c29ac146b11eb47339730137c5
Matthew <12hughesm@gmail.com>
2023-11-30 10:20:26 +0000

    Finished the first task :)

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
 import "./styles/style.scss";
+import {pokemonArray} from "./data/pokemon"
+
+const cardContainer = document.querySelector(".card-container");
+if(!cardContainer) throw new Error("Query error");
+
+const addPokemonCard = (pokemon : Pokemon) => {
+    cardContainer.innerHTML += `
+    <div class="card">
+    <img src="${pokemon.sprite}"/>
+        <div class="card__content">
+            <h1 class="card__heading">${pokemon.name}</h1>
+            <p class="card__text">
+            ${pokemon.name} (${pokemon.id}) is a ${pokemon.types} type pokemon.
+            </p>
+        </div>
+    </div>`
+}
+
+pokemonArray.forEach(pokemon =>{
+    addPokemonCard(pokemon);
+})
+
